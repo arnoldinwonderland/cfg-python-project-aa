3b8e6246fe501729c1d53bf7e18c7bb93d6646c5
Alice <arnoldinwonderland@gmail.com>
2019-03-20 20:00:02 +0000

    CAT.API  session6

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def say_hello_to():
-return render_template("index.html")
+def say_hello():
+ return render_template("index.html") 
+
+@app.route("/<name>")
+def say_hi(name):
+ return f"hello {name}"
 
 @app.route("/<name>")
 def say_hello_to(name):
-  return f"hello {name}""
+  return render_template("index.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+  # request.values is a dictionary holding any
+  # POST request data that's not already part of the URL
+  data = request.values
+
+  return render_template("feedback.html", form_data=data)
 
 app.run(debug=True)
 
